340d3ad6cd2198e7283f6be7294135ed363bff4d
G136 <56437990+G136@users.noreply.github.com>
2025-09-09 22:06:01 +0700

    ugh
    task 3: handle whitespace (and tab?) in filenames

--- solution/task3.sh
+++ solution/task3.sh
@@ -3,4 +3,14 @@
 set -e
 
 
+# make whitespaces work in filenames (dont split on space)
+IFS='
+'
+# ive tried to make newlines work but couldnt
+
 basename --multiple $(find . -type f -iname '*.txt') | sort
+
+# i know theres this
+# but sort breaks newlines anyways soo
+# ill keep the --multiple solution cuz its a cool new option i learnt about
+#find . -type f -iname '*.txt' -exec basename {} ';' | sort
