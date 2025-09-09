5e9da43953a4619ce7808813970a9f664c589428
G136 <56437990+G136@users.noreply.github.com>
2025-09-09 20:22:02 +0700

    очень грустно

--- solution/task1.sh
+++ solution/task1.sh
@@ -6,11 +6,8 @@ set -e
 TMP_DIR="$(mktemp --directory)"
 
 
-(cd "$TMP_DIR"; mkdir 1 2)
-
-
-tar -xf "./data/archive-part1.tar" -C "${TMP_DIR}/1/"
-unzip   "./data/archive-part2.zip" -d "${TMP_DIR}/2/"
+tar -xf "./data/archive-part1.tar" -C "${TMP_DIR}/"
+unzip   "./data/archive-part2.zip" -d "${TMP_DIR}/"
 
 tar -caf "./data/archive-combined.tar.gz" -C "$TMP_DIR" .
 
