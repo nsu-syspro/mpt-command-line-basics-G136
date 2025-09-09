93a901306d7532b991577f59f6e460beb8c457e4
G136 <56437990+G136@users.noreply.github.com>
2025-09-09 23:05:01 +0700

    ahaha

--- solution/task3.sh
+++ solution/task3.sh
@@ -3,12 +3,12 @@
 set -e
 
 
-# make whitespaces work in filenames (dont split on space)
-IFS='
-'
+IFS=$'\n' # make whitespaces work in filenames (dont split on space)
+set -f    # make wildcard symbols work in filenames (turn off globbing)
 # ive tried to make newlines work but couldnt
 
 basename --multiple $(find . -type f -iname '*.txt') | sort
+
 
 # i know theres this
 # but sort breaks newlines anyways soo
