8664479c91476279d4e5c53235074b498d5368cb
G136 <56437990+G136@users.noreply.github.com>
2025-09-09 21:33:21 +0700

    shut up

--- solution/task1.sh
+++ solution/task1.sh
@@ -6,8 +6,8 @@ set -e
 TMP_DIR="$(mktemp --directory)"
 
 
-tar -xf "./data/archive-part1.tar" -C "${TMP_DIR}/"
-unzip   "./data/archive-part2.zip" -d "${TMP_DIR}/"
+tar -xf  "./data/archive-part1.tar" -C "${TMP_DIR}/"
+unzip -q "./data/archive-part2.zip" -d "${TMP_DIR}/"
 
 tar -caf "./data/archive-combined.tar.gz" -C "$TMP_DIR" .
 
